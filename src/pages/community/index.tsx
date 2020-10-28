dcabb0c4455bcd084a1bf6804ed3d17760a3fb4b
comsince <liaojinlong@meizu.com>
2020-10-28 16:53:17 +0800

    更新微信公众号二维码

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -56,7 +56,7 @@ const CareersPage = () => {
               <img
                 alt="A code editor containing a SQL statement"
                 className={careersStyles.card__illustration}
-                src="https://media.comsince.cn/minio-bucket-image-name/1-373z3zNN-1603354959057-qrcode_for_gh_17458ec142b0_1280.jpg"
+                src="https://media.comsince.cn/minio-bucket-image-name/wx-qr.png"
               />
             </div>
           </div>
