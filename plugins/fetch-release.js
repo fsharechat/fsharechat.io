05b6b5f8d4d7bfa01f2e1cca15b00de053217837
comsince <liaojinlong@meizu.com>
2020-11-05 11:04:43 +0800

    发布0.0.18版本

--- plugins/fetch-release.js
+++ plugins/fetch-release.js
@@ -7,19 +7,19 @@ module.exports = (context) => ({
     //   `https://api.github.com/repos/questdb/questdb/releases/tags/${context.siteConfig.customFields.version}`,
     // )
     const data1 = {
-      html_url: 'https://api.github.com/repos/fsharechat/electron-vue-chat/releases/tags/v0.0.15',
-      published_at: '2020-10-02T13:52:20Z',
+      html_url: 'https://api.github.com/repos/fsharechat/electron-vue-chat/releases/tags/v0.0.18',
+      published_at: '2020-11-05T13:52:20Z',
       assets:[{
         browser_download_url:'https://media.comsince.cn/minio-bucket-file-name/fshare-chat-linux.AppImage',
-        name:'fsharechat-0.0.15-rt-linux-amd64.appImage',
+        name:'fsharechat-0.0.18-rt-linux-amd64.appImage',
         size:119835292
       },{
         browser_download_url:'https://media.comsince.cn/minio-bucket-file-name/fshare-chat-macos.dmg',
-        name:'fsharechat-0.0.15-rt-osx-amd64.dmg',
+        name:'fsharechat-0.0.18-rt-osx-amd64.dmg',
         size:117980685
       },{
         browser_download_url:'https://media.comsince.cn/minio-bucket-file-name/fshare-chat-window.exe',
-        name:'fsharechat-0.0.15-rt-windows-amd64.exe',
+        name:'fsharechat-0.0.18-rt-windows-amd64.exe',
         size:85512736
       }]
     };
