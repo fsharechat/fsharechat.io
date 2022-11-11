2dfc1f9b07d2e2122cc831cc42da3065c3425924
comsince <liaojinlong@meizu.com>
2022-11-11 10:09:40 +0800

    修改飞享web在线体验地址

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -163,7 +163,7 @@ const Jumbotron = () => {
         <div className={jumbotronStyles.jumbotron__cta}>
           <Button
             className={jumbotronStyles.jumbotron__link}
-            href="https://chat.comsince.cn"
+            href="https://web.fsharechat.cn"
           >
             在线体验
           </Button>
